20fb72cef549bc39371a48b631049a843f50e704
Gustbel <gustavobelbruno@gmail.com>
2023-09-04 17:16:44 -0300

    added range query

--- app.js
+++ app.js
@@ -21,6 +21,35 @@ app.get('/simple/token_price/:network', (req, res) => {
   res.json(response);
 });
 
+app.get('/coins/contract/:address/market_chart/range', (req, res) => {
+  const address = req.params.address;
+  const vsCurrency = req.query.vs_currency;
+  const from = req.query.from;
+  const to = req.query.to;
+
+  // Implementa la lógica para obtener los datos de mercado según el contrato y el rango de tiempo
+
+  const responseData = {
+    prices: [
+      [1693854122128, 1855.727988281606],
+      [1693854370419, 1853.0540327201713],
+      [1693854701571, 1853.791320764839]
+    ],
+    market_caps: [
+      [1693854122128, 0.0],
+      [1693854370419, 0.0],
+      [1693854701571, 0.0]
+    ],
+    total_volumes: [
+      [1693854122128, 31512018.50055088],
+      [1693854370419, 31455834.45837046],
+      [1693854701571, 31467919.52530859]
+    ]
+  };
+
+  res.json(responseData);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
